efeabfe0c3332d24bfce5a583d5e5488120b0829
vedge <vedge@b83589b1-170a-47ad-b9cd-3de6c41813fc>
2009-04-01 08:37:32 +0000

    driver for standard lpt used as gpio(4) bus

---
+++ trunk/sys/dev/ic/lptgpiovar.h
@@ -0,0 +1,19 @@
+/*	$FabBSD$	*/
+/*	Public domain	*/
+
+#define LPTGPIO_NPINS		17
+
+struct lptgpio_softc {
+	struct device		sc_dev;
+
+	bus_space_tag_t		sc_iot;
+	bus_space_handle_t	sc_ioh;
+
+	struct gpio_chipset_tag	sc_gpio_gc;
+	struct gpio_pin		sc_gpio_pins[LPTGPIO_NPINS];
+};
+
+void	lptgpio_attach_common(struct device *, struct device *, void *);
+int	lptgpio_pin_read(void *, int);
+void	lptgpio_pin_write(void *, int, int);
+void	lptgpio_pin_ctl(void *, int, int);
