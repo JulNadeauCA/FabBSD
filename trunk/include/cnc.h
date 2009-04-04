fa6e6b7d2270c8ef3ca672e1b4cdd2673f5c2114
vedge <vedge@b83589b1-170a-47ad-b9cd-3de6c41813fc>
2009-04-04 02:50:07 +0000

    update for new libcnc

--- trunk/include/cnc.h
+++ trunk/include/cnc.h
@@ -3,13 +3,25 @@
 #ifndef _CNC_H_
 #define _CNC_H_
 
+#include <sys/types.h>
 #include <sys/cnc.h>
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
-const char	*CNC_GetError(void);
-void		 CNC_SetError(const char *);
-int		 CNC_Open(const char *);
-void		 CNC_Close(void);
+const char *cnc_get_error(void);
+void        cnc_set_error(const char *, ...);
+
+int         cnc_vec_parse(cnc_vec_t *, const char *);
+void        cnc_vec_zero(cnc_vec_t *);
+void        cnc_vec_sub(cnc_vec_t *, const cnc_vec_t *, const cnc_vec_t *);
+void        cnc_vec_add(cnc_vec_t *, const cnc_vec_t *, const cnc_vec_t *);
+cnc_real_t  cnc_vec_dotprod(const cnc_vec_t *, const cnc_vec_t *);
+cnc_real_t  cnc_vec_length(const cnc_vec_t *);
+cnc_real_t  cnc_vec_distance(const cnc_vec_t *, const cnc_vec_t *);
+int         cnc_vec_print(const cnc_vec_t *, char *, size_t)
+                __attribute__((__bounded__(__string__,2,3)));
+
+int         cnc_vel_parse(cnc_vel_t *, const char *);
+int         cnc_dist_parse(cnc_dist_t *, const char *);
 __END_DECLS
 #endif /* _CNC_H_ */
